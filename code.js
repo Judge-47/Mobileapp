73235004b93f28659a97712ce74b4e7ca0ab148e
Judge-47 <63011238@kmitl.ac.th>
2023-02-23 11:57:12 +0700

    update files

--- code.js
+++ code.js
@@ -1,13 +1,12 @@
 //fetch the button from the DOM
-var button = document.querySelector(“button”);
+var button = document.querySelector("button");
+
 //attach and event when the user clicks it
-button.addEventListener(“click”, myfunction);
-//create the function that will be called when the
-button is pressed
-function myfunction()
-{
+button.addEventListener("click", myfunction);
 
-//this shows a popup window
-alert(“button clicked!”);
+//create the function that will be called when the button is pressed
+function myfunction() {
 
+    //this shows a popup window
+    alert("button clicked!");
 }
